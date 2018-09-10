c0caf15910a920afda46ea9ad19784a6e4f9e8de
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-09-09 23:26:22 -0500

    Implement PriorityBinding.

---
+++ src/Avalonia.Base/Data/PriorityBinding.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using Avalonia.Metadata;
+using Avalonia.Reactive;
+
+namespace Avalonia.Data
+{
+    /// <summary>
+    /// Binding that produces values from the earliest child binding in the list that has produced values.
+    /// </summary>
+    public class PriorityBinding : IBinding
+    {
+        /// <summary>
+        /// Gets the collection of child bindings.
+        /// </summary>
+        [Content]
+        public IList<IBinding> Bindings { get; set; } = new List<IBinding>();
+        
+        /// <summary>
+        /// Gets or sets the value to use when the binding is unable to produce a value.
+        /// </summary>
+        public object FallbackValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the binding mode.
+        /// </summary>
+        public BindingMode Mode { get; set; } = BindingMode.OneWay;
+
+        /// <summary>
+        /// Gets or sets the binding priority.
+        /// </summary>
+        public BindingPriority Priority { get; set; }
+
+        public InstancedBinding Initiate(IAvaloniaObject target, AvaloniaProperty targetProperty, object anchor = null, bool enableDataValidation = false)
+        {
+            var targetType = targetProperty?.PropertyType ?? typeof(object);
+            var children = Bindings.Select(x => x.Initiate(target, null).Subject);
+
+            var subject = new PrioritySubject(children.ToArray());
+            var mode = Mode == BindingMode.Default ?
+                targetProperty?.GetMetadata(target.GetType()).DefaultBindingMode : Mode;
+
+            switch (mode)
+            {
+                case BindingMode.OneWay:
+                    return InstancedBinding.OneWay(subject, Priority);
+                case BindingMode.TwoWay:
+                    return InstancedBinding.TwoWay(subject, Priority);
+                case BindingMode.OneTime:
+                    return InstancedBinding.OneTime(subject, Priority);
+                case BindingMode.OneWayToSource:
+                    return InstancedBinding.OneWayToSource(subject, Priority);
+                default:
+                    throw new NotSupportedException(
+                        "Unknown BindingMode for PriorityBinding.");
+            }
+        }
+    }
+}
---
+++ src/Avalonia.Base/Reactive/PrioritySubject.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Avalonia.Data;
+
+namespace Avalonia.Reactive
+{
+    public class PrioritySubject : LightweightObservableBase<object>, ISubject<object>
+    {
+        private readonly IReadOnlyList<ISubject<object>> _subjects;
+        private IDisposable subscriptions;
+        private int _activeIndex;
+        private object _lock;
+
+        public PrioritySubject(IReadOnlyList<ISubject<object>> subjects)
+        {
+            _subjects = subjects;
+            _activeIndex = _subjects.Count - 1;
+        }
+
+        public void OnCompleted()
+        {
+            lock (_lock)
+            {
+                _subjects[_activeIndex].OnCompleted();
+            }
+        }
+
+        public void OnError(Exception error)
+        {
+            lock (_lock)
+            {
+                _subjects[_activeIndex].OnError(error);
+            }
+        }
+
+        public void OnNext(object value)
+        {
+            lock (_lock)
+            {
+                _subjects[_activeIndex].OnNext(value);
+            }
+        }
+
+        protected override void Deinitialize()
+        {
+            subscriptions.Dispose();
+        }
+
+        protected override void Initialize()
+        {
+            subscriptions = new CompositeDisposable(
+                _subjects
+                    .Select((sub, i) =>
+                        sub
+                            .Where(o => o != AvaloniaProperty.UnsetValue)
+                            .Subscribe(o => InnerSubjectOnNext(i, o))));
+        }
+
+        private void InnerSubjectOnNext(int index, object value)
+        {
+            if (index < _activeIndex)
+            {
+                lock (_lock)
+                {
+                    if (index < _activeIndex && value != BindingOperations.DoNothing)
+                    {
+                        _activeIndex = index;
+                        PublishNext(value);
+                    }
+                }
+            }
+        }
+    }
+}
