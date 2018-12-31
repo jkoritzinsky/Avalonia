f51a4c0b97ef89b5f430aa45b2bc09aed82d7d65
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-12-30 22:48:58 -0600

    Port RemoteWidget input handling from AvalonStudio.

--- src/Avalonia.Controls/Remote/RemoteWidget.cs
+++ src/Avalonia.Controls/Remote/RemoteWidget.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Remote.Protocol;
+using Avalonia.Remote.Protocol.Input;
 using Avalonia.Remote.Protocol.Viewport;
 using Avalonia.Threading;
 using PixelFormat = Avalonia.Platform.PixelFormat;
@@ -34,6 +38,10 @@ namespace Avalonia.Controls.Remote
                     Avalonia.Remote.Protocol.Viewport.PixelFormat.Rgba8888,
                 }
             });
+
+            AddHandler(KeyUpEvent, OnKeyUp, RoutingStrategies.Tunnel);
+            AddHandler(KeyDownEvent, OnKeyDown, RoutingStrategies.Tunnel);
+            AddHandler(TextInputEvent, OnTextInput, RoutingStrategies.Tunnel);
         }
 
         public SizingMode Mode { get; set; }
@@ -87,6 +95,148 @@ namespace Avalonia.Controls.Remote
                     new Rect(Bounds.Size));
             }
             base.Render(context);
+        }
+
+        protected override void OnPointerPressed(PointerPressedEventArgs e)
+        {
+            var point = e.GetPosition(this);
+
+            _connection.Send(new PointerPressedEventMessage
+            {
+                Modifiers = ToAvaloniaModifiers(e.InputModifiers),
+                X = point.X,
+                Y = point.Y,
+                Button = (Avalonia.Remote.Protocol.Input.MouseButton)e.MouseButton
+            });
+
+            base.OnPointerPressed(e);
+        }
+
+        protected override void OnPointerReleased(PointerReleasedEventArgs e)
+        {
+            var point = e.GetPosition(this);
+
+            _connection.Send(new PointerReleasedEventMessage
+            {
+                Modifiers = ToAvaloniaModifiers(e.InputModifiers),
+                X = point.X,
+                Y = point.Y,
+                Button = (Avalonia.Remote.Protocol.Input.MouseButton)e.MouseButton
+            });
+
+            base.OnPointerReleased(e);
+        }
+
+        protected override void OnPointerWheelChanged(PointerWheelEventArgs e)
+        {
+            var point = e.GetPosition(this);
+
+            _connection.Send(new ScrollEventMessage
+            {
+                Modifiers = ToAvaloniaModifiers(e.InputModifiers),
+                X = point.X,
+                Y = point.Y,
+                DeltaX = e.Delta.X,
+                DeltaY = e.Delta.Y
+            });
+
+            base.OnPointerWheelChanged(e);
+        }
+
+        protected override void OnPointerMoved(PointerEventArgs e)
+        {
+            var point = e.GetPosition(this);
+
+            _connection.Send(new PointerMovedEventMessage
+            {
+                Modifiers = ToAvaloniaModifiers(e.InputModifiers),
+                X = point.X,
+                Y = point.Y
+            });
+
+            base.OnPointerMoved(e);
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            _connection.Send(new KeyEventMessage
+            {
+                IsDown = true,
+                Key = (Avalonia.Remote.Protocol.Input.Key)e.Key,
+                Modifiers = ToAvaloniaModifiers(e.Modifiers)
+            });
+
+            e.Handled = true;
+
+            base.OnKeyDown(e);
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            _connection.Send(new KeyEventMessage
+            {
+                IsDown = false,
+                Key = (Avalonia.Remote.Protocol.Input.Key)e.Key,
+                Modifiers = ToAvaloniaModifiers(e.Modifiers)
+            });
+
+            e.Handled = true;
+
+            base.OnKeyUp(e);
+        }
+
+        private void OnTextInput(object sender, TextInputEventArgs e)
+        {
+            _connection.Send(new TextInputEventMessage
+            {
+                Text = e.Text
+            });
+
+            e.Handled = true;
+
+            base.OnTextInput(e);
+        }
+
+        private static Avalonia.Remote.Protocol.Input.InputModifiers[] ToAvaloniaModifiers(Avalonia.Input.InputModifiers modifiers)
+        {
+            var result = new List<Avalonia.Remote.Protocol.Input.InputModifiers>();
+
+            if ((modifiers & Avalonia.Input.InputModifiers.Control) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.Control);
+            }
+
+            if ((modifiers & Avalonia.Input.InputModifiers.Alt) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.Alt);
+            }
+
+            if ((modifiers & Avalonia.Input.InputModifiers.Shift) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.Shift);
+            }
+
+            if ((modifiers & Avalonia.Input.InputModifiers.Windows) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.Windows);
+            }
+
+            if ((modifiers & Avalonia.Input.InputModifiers.LeftMouseButton) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.LeftMouseButton);
+            }
+
+            if ((modifiers & Avalonia.Input.InputModifiers.RightMouseButton) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.RightMouseButton);
+            }
+
+            if ((modifiers & Avalonia.Input.InputModifiers.MiddleMouseButton) != 0)
+            {
+                result.Add(Avalonia.Remote.Protocol.Input.InputModifiers.MiddleMouseButton);
+            }
+
+            return result.ToArray();
         }
     }
 }
